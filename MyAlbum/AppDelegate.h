b36edd1b6634b4a23c4c175d71044fcb2c0c2f15
DDT <DDT@DDTdeMacBook-Pro.local>
2014-09-16 18:34:28 +0800

    Initial Commit

---
+++ MyAlbum/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyAlbum
+//
+//  Created by DDT on 2014/9/16.
+//  Copyright (c) 2014年 DDT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyAlbum/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyAlbum
+//
+//  Created by DDT on 2014/9/16.
+//  Copyright (c) 2014年 DDT. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
